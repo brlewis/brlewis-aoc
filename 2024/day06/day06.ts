d9771afeb8d35c3548c082f21dc8e4e71a3cdf67
Bruce Lewis <brlewis@alum.mit.edu>
2024-12-06 10:42:29 -0500

    2024 day 6 part 2

--- 2024/day06/day06.ts
+++ 2024/day06/day06.ts
@@ -1,8 +1,8 @@
 import { Grid } from "../../util.ts";
 
-export const helper = (input: string) => {
-  const grid = new Grid(input);
+export const helper = (grid: Grid, obstacle?: number) => {
   const positions = new Set<number>();
+  const seen = new Map<number, Set<number>>();
   const directions = [[0, -1], [1, 0], [0, 1], [-1, 0]];
   let direction = 0;
   let [dx, dy] = directions[0];
@@ -10,26 +10,42 @@ export const helper = (input: string) => {
     direction = (direction + 1) % 4;
     [dx, dy] = directions[direction];
   };
-  let start = input.indexOf("^"); // works only if guard starts out facing north
+  let start = grid.contents.indexOf("^"); // assumes north-facing start
   let [x, y] = [grid.x(start), grid.y(start)];
   while (grid.inside(x, y)) {
-    positions.add(grid.index(x, y));
+    const pos = grid.index(x, y);
+    if (positions.has(pos)) {
+      let dirs = seen.get(pos) ?? new Set<number>();
+      if (dirs.has(direction)) {
+        return { positions, loop: true };
+      }
+      dirs.add(direction);
+      seen.set(pos, dirs);
+    }
+    positions.add(pos);
     const [newX, newY] = [x + dx, y + dy];
     const what = grid.get(newX, newY);
-    if (what === "#") {
+    if (what === "#" || obstacle === grid.index(newX, newY)) {
       turn();
     } else {
       [x, y] = [newX, newY];
     }
   }
-  return { grid, positions };
+  return { positions, loop: false };
 };
 
 export const aoc24_06_01 = (input: string) => {
-  return helper(input).positions.size;
+  return helper(new Grid(input)).positions.size;
 };
 
 export const aoc24_06_02 = (input: string) => {
-  const { grid, positions } = helper(input);
+  const grid = new Grid(input);
+  const { positions } = helper(grid);
   let count = 0;
+  positions.forEach((pos) => {
+    if (helper(grid, pos).loop) {
+      count++;
+    }
+  });
+  return count;
 };
--- 2024/day06/day06_test.ts
+++ 2024/day06/day06_test.ts
@@ -1,10 +1,16 @@
 import { describe, it } from "bdd";
 import { expect } from "expect";
-import { aoc24_06_01 } from "./day06.ts";
+import { aoc24_06_01, aoc24_06_02 } from "./day06.ts";
 
 describe("2024 day6 part 1", () => {
   it("solves the puzzle", () => {
     expect(aoc24_06_01(puzzleInput || sampleInput)).toEqual(41);
+  });
+});
+
+describe("2024 day6 part 2", () => {
+  it("solves the puzzle", () => {
+    expect(aoc24_06_02(puzzleInput || sampleInput)).toEqual(6);
   });
 });
 
