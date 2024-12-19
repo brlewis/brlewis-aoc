d51befbafb263850fd3e38930251c16143cb4450
Bruce Lewis <brlewis@alum.mit.edu>
2024-12-19 09:11:47 -0500

    2024 day 19 part 2

--- 2024/day19/day19.ts
+++ 2024/day19/day19.ts
@@ -1,4 +1,4 @@
-export const aoc24_19 = (input: string) => {
+export const aoc24_19_1 = (input: string) => {
   const rejects = new Set<string>();
   const [towelStr, patternStr] = input.split("\n\n");
   const towels = new Set(towelStr.split(", "));
@@ -37,4 +37,46 @@ export const aoc24_19 = (input: string) => {
     }
   }
   return count;
+};
+
+export const aoc24_19_2 = (input: string) => {
+  const cache = new Map<string, number>();
+  const rejects = new Set<string>();
+  const [towelStr, patternStr] = input.split("\n\n");
+  const towels = new Set(towelStr.split(", "));
+  const patterns = patternStr.split("\n");
+  function combos(pattern: string, start: number) {
+    if (start === pattern.length) {
+      return 1;
+    }
+    const whole = pattern.slice(start, pattern.length);
+    const cached = cache.get(whole);
+    if (cached !== undefined) {
+      return cached;
+    }
+    let count = 0;
+    for (
+      let stripes = pattern.length - start;
+      stripes > 0;
+      stripes--
+    ) {
+      if (towels.has(pattern.slice(start, start + stripes))) {
+        const rest = combos(pattern, start + stripes);
+        const candidate = pattern.slice(start + stripes);
+        if (rest) {
+          cache.set(candidate, rest);
+          count += rest;
+        } else {
+          rejects.add(candidate);
+        }
+      }
+    }
+    cache.set(whole, count);
+    return count;
+  }
+  let sum = 0;
+  for (let i = 0; i < patterns.length; i++) {
+    sum += combos(patterns[i], 0);
+  }
+  return sum;
 };
--- 2024/day19/day19_test.ts
+++ 2024/day19/day19_test.ts
@@ -1,13 +1,22 @@
 import { describe, it } from "bdd";
 import { expect } from "expect";
-import { aoc24_19 } from "./day19.ts";
+import { aoc24_19_1, aoc24_19_2 } from "./day19.ts";
 
 describe("2024 day19 part 1", () => {
   it("solves the sample", () => {
-    expect(aoc24_19(sampleInput)).toEqual(6);
+    expect(aoc24_19_1(sampleInput)).toEqual(6);
   });
   it("solves the puzzle", () => {
-    expect(aoc24_19(puzzleInput || sampleInput)).toEqual(6);
+    expect(aoc24_19_1(puzzleInput || sampleInput)).toEqual(6);
+  });
+});
+
+describe("2024 day19 part 2", () => {
+  it("solves the sample", () => {
+    expect(aoc24_19_2(sampleInput)).toEqual(16);
+  });
+  it("solves the puzzle", () => {
+    expect(aoc24_19_2(puzzleInput || sampleInput)).toEqual(16);
   });
 });
 
